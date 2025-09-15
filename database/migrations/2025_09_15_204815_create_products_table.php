51857c7a4346b42342a9ab738efbef8c4eccd9c3
codejutsu1 <codejutsu@protonmail.com>
2025-09-15 21:53:36 +0100

    Created the product model

---
+++ database/migrations/2025_09_15_204815_create_products_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Models\Vendor;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('uuid');
+
+            $table->foreignIdFor(Vendor::class);
+
+            $table->string('name');
+            $table->string('description');
+            $table->string('price');
+            $table->string('status');
+
+            $table->boolean('is_addon')->default(false);
+            $table->string('type');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
