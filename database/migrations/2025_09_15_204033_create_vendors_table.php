30ce1de890991291ef8a882e4814d1d1dc73f3a0
codejutsu1 <codejutsu@protonmail.com>
2025-09-15 21:47:09 +0100

    worked on the vendors table

---
+++ database/migrations/2025_09_15_204033_create_vendors_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vendors', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('uuid');
+
+            $table->foreignIdFor(User::class);
+
+            $table->string('name');
+            $table->string('email');
+            $table->string('phone');
+            $table->string('address');
+            $table->string('website')->nullable();
+            $table->string('description')->nullable();
+            $table->string('status');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vendors');
+    }
+};
