7cd9d3ae02721f0a061b668a7727e305450097fb
codejutsu1 <codejutsu@protonmail.com>
2025-09-15 21:58:56 +0100

    Created the cart item model

---
+++ database/migrations/2025_09_15_205757_create_cart_items_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Models\Cart;
+use App\Models\Product;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cart_items', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('uuid');
+
+            $table->foreignIdFor(Cart::class);
+
+            $table->foreignIdFor(Product::class);
+
+            $table->integer('quantity');
+            $table->string('status');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cart_items');
+    }
+};
