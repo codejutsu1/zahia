ce32ab2cf22893917997e82c890b429a0c7834ca
codejutsu1 <codejutsu@protonmail.com>
2025-09-15 22:05:30 +0100

    created the order model

---
+++ database/migrations/2025_09_15_205948_create_orders_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Models\Cart;
+use App\Models\User;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('uuid');
+
+            $table->foreignIdFor(User::class);
+            $table->foreignIdFor(Cart::class);
+
+            $table->string('total_amount');
+
+            $table->string('status');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
