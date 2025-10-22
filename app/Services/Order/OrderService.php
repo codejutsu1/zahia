a1528be67338f23f41ddf0d8643990e6e0456a2d
codejutsu1 <codejutsu@protonmail.com>
2025-10-22 18:40:29 +0100

    worked on the repeat order functionslity

--- app/Services/Order/OrderService.php
+++ app/Services/Order/OrderService.php
@@ -2,9 +2,13 @@
 
 namespace App\Services\Order;
 
+use App\Enums\CartStatus;
+use App\Models\Cart;
+use App\Models\CartItem;
 use App\Models\Order;
 use App\Services\Order\Actions\CreateOrderAction;
 use App\Services\Order\Data\CreateOrderData;
+use Illuminate\Support\Str;
 
 class OrderService
 {
@@ -27,9 +31,34 @@ class OrderService
         // $vendor->notify(new OrderCreated($order));
     }
 
-    public function repeatOrder(Order $order): void
+    public function repeatOrder(Order $order): Cart
     {
-        //
+        /** @var Cart $cart */
+        $cart = $order->cart;
+
+        $newCart = Cart::create([
+            'user_id' => $order->user_id,
+            'vendor_id' => $cart->vendor_id,
+            'status' => CartStatus::ACTIVE,
+        ]);
+
+        /** @phpstan-ignore-next-line */
+        $cartItemData = $cart->items->map(function (CartItem $cartItem) use ($newCart) {
+            return [
+                'uuid' => Str::uuid(),
+                'cart_id' => $newCart->id,
+                'product_id' => $cartItem->product_id,
+                'quantity' => $cartItem->quantity,
+                'is_addon' => $cartItem->is_addon,
+                'status' => $cartItem->status,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ];
+        })->toArray();
+
+        CartItem::insert($cartItemData);
+
+        return $newCart;
     }
 
     public function scheduleOrder(Order $order): void
