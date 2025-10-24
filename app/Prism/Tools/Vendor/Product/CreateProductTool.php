aa60e0ede896186d00634d241cd731da8ed17af8
codejutsu1 <codejutsu@protonmail.com>
2025-10-24 20:07:51 +0100

    created a vendor class for create product tool

---
+++ app/Prism/Tools/Vendor/Product/CreateProductTool.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Prism\Tools\Vendor\Product;
+
+class CreateProductTool
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
