f31aedbb03b69a9cc60f8fe0c6c1fd2a0ead6489
codejutsu1 <codejutsu@protonmail.com>
2025-11-23 14:55:37 +0100

    fix the pest issue

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Services\Chatbot\ChatbotManager;
+use App\Services\Llm\LlmManager;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +13,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->singleton(LlmManager::class, function ($app) {
+            return new LlmManager($app);
+        });
+
+        $this->app->singleton(ChatbotManager::class, function ($app) {
+            return new ChatbotManager($app);
+        });
     }
 
     /**
