c98d3f5bebc7a0da7e7ffbf4400f9ab08560d151
codejutsu1 <codejutsu@protonmail.com>
2025-11-23 15:06:57 +0100

    remove some variables in the app service providers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,6 @@
 
 namespace App\Providers;
 
-use App\Services\Chatbot\ChatbotManager;
-use App\Services\Llm\LlmManager;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,13 +11,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        $this->app->singleton(LlmManager::class, function ($app) {
-            return new LlmManager($app);
-        });
-
-        $this->app->singleton(ChatbotManager::class, function ($app) {
-            return new ChatbotManager($app);
-        });
+        //
     }
 
     /**
