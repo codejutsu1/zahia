b10548b8a894ba60ab1f35ba17f09dccaeaf58c5
codejutsu1 <codejutsu@protonmail.com>
2025-12-06 19:20:36 +0100

    worked on the delivery model

---
+++ app/Models/Delivery.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use App\Enums\DeliveryStatus;
+use App\Traits\HasUuidColumn;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Delivery extends Model
+{
+    /** @use HasFactory<\Database\Factories\DeliveryFactory> */
+    use HasFactory;
+
+    use HasUuidColumn;
+
+    protected $fillable = [
+        'uuid',
+        'order_id',
+        'address',
+        'status',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'status' => DeliveryStatus::class,
+        ];
+    }
+
+    public function order(): BelongsTo
+    {
+        return $this->belongsTo(Order::class);
+    }
+}
