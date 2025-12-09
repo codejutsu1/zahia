033759c7e31015ad7e6629500d9887e77db45b90
codejutsu1 <codejutsu@protonmail.com>
2025-12-09 16:04:59 +0100

    using resend for email

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Mail\OrderShipped;
 use App\Models\Order;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -11,7 +12,8 @@ Route::get('/', function () {
 Route::get('/mail', function () {
     $order = Order::find(1);
 
-    return new OrderShipped($order);
+    // return new OrderShipped($order);
+    Mail::to('danieldunu001@gmail.com')->send(new OrderShipped($order));
 });
 
 require __DIR__.'/webhooks.php';
